b0641e09388292e2161c73069cbf187b8e5e159f
ukkodeveloper <ukkodeveloper@gmail.com>
2023-06-13 11:18:31 +0900

    refactor: lc 5

--- leetcode/5.js
+++ leetcode/5.js
@@ -1,44 +1,48 @@
-const getSubPalindrome = (left, right, str) => {
-  let startIndex = left;
-  let endIndex = right;
+// 마음에 안 드는 점
+// 1. longestPalindrome for 반복 수행
+// 2. getSubPalindrome 함수에서 반환시에 인덱스를 하나씩 줄이는 것이 마음에 들지 않는다.
 
-  while (
-    startIndex >= 0 &&
-    endIndex <= str.length - 1 &&
-    str[startIndex] === str[endIndex]
-  ) {
-    startIndex -= 1;
-    endIndex += 1;
+const expandAroundCenter = (str, left, right) => {
+  while (left >= 0 && right <= str.length && str[left] === str[right]) {
+    left -= 1;
+    right += 1;
   }
 
   return {
-    subStartIndex: startIndex + 1,
-    subLength: endIndex - startIndex - 1,
+    start: left + 1,
+    len: right - left - 1,
   };
 };
 
 const longestPalindrome = (str) => {
   if (str.length === 1) return str;
 
-  let maxLength = 1;
-  let startIndex = 0;
+  let start = 0;
+  let maxLen = 1;
 
   for (let i = 1; i < str.length; i++) {
-    const { subStartIndex, subLength } = getSubPalindrome(i - 1, i + 1, str);
+    const { start: evenStart, len: evenLen } = expandAroundCenter(
+      str,
+      i,
+      i + 1
+    );
 
-    if (subLength > maxLength) {
-      maxLength = subLength;
-      startIndex = subStartIndex;
+    const { start: oddStart, len: oddLen } = expandAroundCenter(
+      str,
+      i - 1,
+      i + 1
+    );
+
+    if (evenLen > maxLen) {
+      start = evenStart;
+      maxLen = evenLen;
+    }
+
+    if (oddLen > maxLen) {
+      start = oddStart;
+      maxLen = oddLen;
     }
   }
 
-  for (let i = 1; i < str.length; i++) {
-    const { subStartIndex, subLength } = getSubPalindrome(i - 1, i, str);
-    if (subLength > maxLength) {
-      maxLength = subLength;
-      startIndex = subStartIndex;
-    }
-  }
-
-  return str.slice(startIndex, startIndex + maxLength);
+  return str.slice(start, start + maxLen);
 };
