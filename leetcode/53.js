c9df369deb433a2982d1f6ac1d1025c7a8e24552
ukkodeveloper <ukkodeveloper@gmail.com>
2023-06-24 13:20:41 +0900

    re: lc 53

--- leetcode/53.js
+++ leetcode/53.js
@@ -1,18 +1,16 @@
 const maxSubArray = (nums) => {
-  if (nums.every((num) => num < 0)) {
-    return Math.max(...nums);
-  }
-
   let maxSum = nums[0];
-  let curSum = 0;
+  let prevSum = nums[0];
 
-  for (const num of nums) {
-    curSum += num;
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
 
-    if (curSum < 0) curSum = 0;
-
-    maxSum = Math.max(maxSum, curSum);
+    prevSum = Math.max(prevSum + num, num);
+    maxSum = Math.max(maxSum, prevSum);
   }
 
   return maxSum;
 };
+
+// ! before: 0을 기준으로 누적값을 시작할 지 정했다. -> 음수만 있을 경우 따로 처리를 해줘야 했음.
+// ! after: 순회하면서 현재값을 더할 지, 새로 시작할 지를 비교해서 정한다.
